81d4efbb3ff345ed8459137d77d12c4e27e7f0a3
Vetle Ledaal <vetle.ledaal@gmail.com>
2020-10-15 16:36:48 +0200

    verify_dataset: show where the errors are

--- verify_dataset.py
+++ verify_dataset.py
@@ -1,8 +1,15 @@
-import os
 import argparse
+import os
+
 from config import SUPERVISED_CLASSES as valid_categories
 
-def verify_single(filename, debug=True):
+
+def assert_continue(expr: bool, err_msg: str, filename: str, index: int):
+    if not expr:
+        print(f'[!] invalid {err_msg}, {filename}:{index+1}')
+
+
+def verify_single(filename: str) -> int:
     counter = 0
     # Load dataset
     with open(filename, mode='r', encoding='utf-8') as f:
@@ -12,21 +19,39 @@ def verify_single(filename, debug=True):
             xs = x.split()
 
             # category must be valid
-            assert cat in valid_categories
+            assert_continue(
+                cat in valid_categories,
+                'category',
+                filename,
+                counter,
+            )
 
             # input must be 10 words
-            assert len(xs) == 10
+            assert_continue(
+                len(xs) == 10,
+                'sentence',
+                filename,
+                counter,
+            )
 
             # output must be 1 word
-            assert ' ' not in y
-
+            assert_continue(
+                ' ' not in y,
+                '11th word',
+                filename,
+                counter,
+            )
             # output must be a string
-            assert type(y) is str
+            assert_continue(
+                ' ' not in y,
+                '11th word type',
+                filename,
+                counter,
+            )
 
             counter += 1
+    return counter
 
-    if debug:
-        print(f'{filename} has {counter} points')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Verify datasets.')
@@ -50,4 +75,6 @@ if __name__ == '__main__':
 
     os.chdir(args.data_dir)
     for filename in args.files:
-        verify_single(filename, debug=args.debug)
+        counter = verify_single(filename)
+        if args.debug:
+            print(f'{filename} has {counter} datapoints')
